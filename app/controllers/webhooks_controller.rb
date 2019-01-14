e31856327cd607f19669771f357c76933e774c8c
Jalerson Lima <jalerson@gmail.com>
2019-01-14 20:48:58 +0100

    Update webhooks_controller.rb

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,6 @@
 class WebhooksController < ApplicationController
   def github
-    if GithubEvent.create secured_params(event)
+    if GithubEvent.create github_event_params
       render json: { success: true }, status: 201
     else
       render json: { success: false }, status: 500
@@ -9,7 +9,7 @@ class WebhooksController < ApplicationController
 
   private
 
-  def secured_params(event)
+  def github_event_params
     {
       github_id: event['headers']['X-GitHub-Delivery'],
       event: event['headers']['X-GitHub-Event'],
